635e2993a45f8fa07ffc0224e8f26dd3e902700a
JackelineGS <jackeline.garcia.serna@gmail.com>
2023-08-15 23:31:30 -0500

    We add functions to activate mouseenter and mouseleaver also we create the front and back cards

--- src/components/project.js
+++ src/components/project.js
@@ -19,36 +19,69 @@ const project = (navigateTo) => {
     </header>
     <main class='projectMain'>
       <div class='aboutProject'>
-        <div class='divOne'>
+        <div class='divProject'>
           <h1 class='proTitle'>Cipher</h1>
-          <div class='projectOne'></div>
+          <div class='card-oneFront'></div>
+          <div class='card-oneBack'>
+            <p>Se creó una página web que permite cifrar y descifrar mensajes, para ello se utilizó el método del cifrado César</p>
+            <div class='buttonDiv'>  
+              <button class='goDemo'>
+                <a href='https://jackelinegs.github.io/DEV005-cipher/src' target='_blank'>Demo</a>
+              </button>
+              <button class='goCode'>
+                <a href='https://github.com/JackelineGS/DEV005-cipher' target='_blank'>Código</a>
+              </button>
+            </div>
+            </div>
           <p class='proDescription'>Tecnologías: HTML, JavaScript, CSS, Git, Github, Jest </p>
         </div>
-        <div class='divTwo'>
+        <div class='divProject'>
           <h1 class='proTitle'>Data Lovers</h1>
-          <div class='projectTwo'></div>
+          <div class='card-twoFront'></div>
+          <div class='card-twoBack'>
+            <p>Se desarrolló una página web para visualizar las películas de Studio Ghibli</p>
+            <div class='buttonDiv'>
+              <button class='goDemo'>
+                <a href='https://jackelinegs.github.io/DEV005-data-lovers/src/' target='_blank'>Demo</a>
+              </button>
+              <button class='goCode'>
+                <a href='https://github.com/JackelineGS/DEV005-data-lovers' target='_blank'>Código</a>
+              </button>
+            </div>
+          </div>
           <p class='proDescription'>Tecnologías: Figma, HTML, JavaScript, CSS, Git, Github, Node.js, Jest </p>
         </div>
-        <div class='divThree'>
+        <div class='divProject'>
           <h1 class='proTitle'>Md-Links</h1>
-          <div class='projectThree'></div>
+          <div class='card-threeFront'></div>
+          <div class='card-threeBack'>Proyecto tres</div>
           <p class='proDescription'>Tecnologías: Figma, HTML, JavaScript, CSS, Git, Github, Node.js, Jest, Firebase </p>
         </div>
-        <div class='divFour'>
+        <div class='divProject'>
           <h1 class='proTitle'>Social Network</h1>
-          <div class='projectFour'></div>
+          <div class='card-fourFront'></div>
+          <div class='card-fourBack'>Proyecto cuatro</div>
           <p class='proDescription'>Tecnologías: JavaScript, Git, Github, Node.js, Axios, Jest </p>
         </div>
-        <div class='divFive'>
+        <div class='divProject'>
           <h1 class='proTitle'>Burguer Queen</h1>
-          <div class='projectFive'></div>
+          <div class='card-fiveFront'></div>
+          <div class='card-fiveBack'>Proyecto cinco</div>
           <p class='proDescription'>Tecnologías: Angular, JavaScript, TypeScript, HTTP, Git, Github, Karma, SASS, Node.js </p>
         </div>
       </div>
     </main>
   `;
 
-  
+  projectDiv.querySelector('.divProject').addEventListener('mouseenter', function () {
+    this.classList.add('card-oneFront');
+  });
+
+  projectDiv.querySelector('.divProject').addEventListener('mouseleave', function() {
+    this.classList.remove('card-oneFront');
+  });
+
+
   const bHome = projectDiv.querySelector('.bHome');
   bHome.addEventListener('click', () => {
       navigateTo('/');
